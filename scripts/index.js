29fe7107e5f6374eb23cf65d75627af2ed16d56b
Svetlana Podgainova <spodgainova@gmail.com>
2024-06-11 23:20:21 +0300

    functionDelete

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,5 @@
 const cardsList = document.querySelector('.places__list');
 
-
 // @todo: Темплейт карточки
 
 const cardsTemplate = document.querySelector('#card-template').content;
@@ -19,15 +18,17 @@ function addCard (card, deleteCard) {
   cardTitle.textContent = card.name;
 
   const deleteButton = cardsItem.querySelector('.card__delete-button');
-  deleteButton.addEventListener('on click', deleteCard);
+  deleteButton.addEventListener('click', function(evt) {
+    deleteCard(cardsItem)
+  });
     
   return cardsItem;
 }
 
 // @todo: Функция удаления карточки
 
-function deleteCard () {
-   cardsList.remove();
+function deleteCard (cardsItem) {
+  cardsItem.remove();
 }
 
 // @todo: Вывести карточки на страницу
